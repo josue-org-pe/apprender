9660921e4621ce6f044714fe054ee84269ce3931
Sandro Chavez <sandrochavez1204@gmail.com>
2025-11-02 17:08:07 -0500

    demo-gym

---
+++ app/src/pages/Gym.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+import Header from "../components/HeaderTemp"
+
+function Gym(){
+
+    const problemss = [
+        {tittle: "Buildings"  , course: "Fisica" , topic: "Dinamic", points: 100},
+        {tittle: "Buildings"  , course: "Fisica" , topic: "Dinamic", points: 100},
+        {tittle: "Buildings"  , course: "Fisica" , topic: "Dinamic", points: 100},  
+        {tittle: "Buildings"  , course: "Fisica" , topic: "Dinamic", points: 100},
+    ]
+
+    return(
+
+        <>
+            <Header/>
+            <main>
+
+            </main>
+        </>
+
+    )
+
+}
