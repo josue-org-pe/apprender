0af7eba4b0c990c630a4fd9868960bb488f75507
Sandro Chavez <sandrochavez1204@gmail.com>
2025-11-03 12:13:57 -0500

    commit-final

--- app/src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth.jsx';
-
-export const PrivateRoute = ({ children }) => {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" replace />;
-};
